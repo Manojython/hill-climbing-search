9cd17067d5fbe8c8c493f9e963bbb8ac86e1ecdc
Christian Micklisch <christian.micklisch@successwithsos.com>
2020-03-15 19:44:07 -0400

    Added test case for valid solution

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 from hill_climbing import *
 
 # test generate_random_solution
-print(generate_random_solution(13))
+generate_random_solution()
+
+# test the evaluate function, ensure that
+# all the valid solutions are 0 and all
+# invalid solutions are greater than 0
+def test_evaluate():
+    valid_solution = [
+        [0,1,0,0],
+        [0,0,0,1],
+        [1,0,0,0],
+        [0,0,1,0]
+    ]
+
+    assert evaluate(valid_solution) == 0
