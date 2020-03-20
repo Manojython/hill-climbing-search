96d3e558a054781f980528c77e062e3f7d466e72
Christian Micklisch <christian.micklisch@successwithsos.com>
2020-03-20 19:13:17 -0400

    Added the test folder

---
+++ test/test.py
@@ -0,0 +1,15 @@
+from test_evaluate import *
+from test_generate import *
+from test_hill_climbing import *
+
+def test_all():
+    test_horizontalAttacks();
+    test_verticalAttacks();
+    test_diagonalAttacks();
+    test_reverseDiagonalAttacks();
+    test_evaluate();
+    test_best_successor();
+    test_generate_initial_state();
+    test_generate_successors();
+
+test_all();
---
+++ test/test_hill_climbing.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import numpy as np
+sys.path.append(os.path.abspath("../"))
+from hill_climbing import *
+
+def test_best_successor():
+    assert type(find_best_successor([])) == Node, "The return is a node"
+    assert find_best_successor([
+            [
+                [0,1,0,0],
+                [0,0,0,1],
+                [1,0,0,0],
+                [0,0,1,0]
+            ],
+            [
+                [0,1,0,0],
+                [0,1,0,0],
+                [1,0,0,0],
+                [0,0,1,0]
+            ]
+        ]).cost == 0, "The node with the lowest score (0) should be picked"
