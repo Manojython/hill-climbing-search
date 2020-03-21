8cd0bf8d50ac11ad01a99079464746111e53f337
mkrish14 <54948283+mkrish14@users.noreply.github.com>
2020-03-20 20:47:42 -0400

    commit

--- generate_nxn.py
+++ generate_nxn.py
@@ -8,61 +8,73 @@ import random
 import numpy as np
 
 def generate_initial_state(n_queens):
-	board = []
-	for i in range(n_queens):
-		queen_place = random.randint(0,n_queens-1)
-		temp = []
-		counter = 0
-		while counter<n_queens:
-			if(counter==queen_place):
-				temp.append(1)
-			else:
-				temp.append(0)
-			counter+=1
+    board = []
+    for i in range(n_queens):
+        queen_place = random.randint(0,n_queens-1)
+        temp = []
+        counter = 0
+        while counter<n_queens:
+            if(counter==queen_place):
+                temp.append(1)
+            else:
+                temp.append(0)
+            counter+=1
 
-		board.append(temp)
-		return_board = []
-		for i in np.rot90(board):
-			return_board.append(list(i))
+        board.append(temp)
+        return_board = []
+        for i in np.rot90(board):
+            return_board.append(list(i))
 
-	return return_board
+    return return_board
+
+def get_next(row):
+    possible_states=[]
+    possible_states.append(row)
+    mat =[0]*len(row)
+    for j in range(len(row)):
+        mat[j] = 1
+        temp = mat[:]
+        if(temp not in possible_states):
+            possible_states.append(temp)
+        mat[j]=0
+    return possible_states
 
 def generate_successors(state):
-	matrix_len = len(state)
-	n_successors = (matrix_len*matrix_len) - matrix_len
-	state = np.rot90(state,axes=(1,0))
-	solution_list = []
-	for i in range(matrix_len):
-		row_name=state[i]
-		counter = 0
-		row_state=[]
-		while counter < len(row_name):
-			if(row_name not in row_state):
-				row_state.append(row_name)
-				counter+=1
-			random.shuffle(row_name)
 
-		for j in range(row_state):
-			state[i] = row_state[j]
-			state = np.rot90(state)
-			solution_list.append(state)
+    matrix_len = len(state)
+    # n_successors = (matrix_len*matrix_len) - matrix_len
+    state = (np.rot90(state,axes=(1,0)))
+    # print(state)
+    solution_list = []
+    for i in range(matrix_len):
+        row_name=(state[i]).tolist()
+        # print(row_name)
+        
+        
+            # random.shuffle(row_name)
+        row_state = get_next(row_name)
 
-	return solution_list
+        for j in range(len(row_state)):
+            state[i] = row_state[j]
+            state = np.rot90(state)
+            solution_list.append(state.tolist())
+
+    return solution_list
 
 
 def generate(n_queens):
-	board= []
-	
-	for i in range(n_queens):
-		temp = []
-		counter = 0
-		while counter<n_queens:
-			temp.append("_")
-			counter+=1
+    board= []
+    
+    for i in range(n_queens):
+        temp = []
+        counter = 0
+        while counter<n_queens:
+            temp.append("_")
+            counter+=1
 
-		board.append(temp)
+        board.append(temp)
 
-	return board
+    return board
 
 
 # print(arr)
--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 import sys
 import os
 
-from test_files import test
+# from test_files import test
+from generate_nxn import generate_successors
 
-test.test_all()
-print("Test cases passed")
+initial_state = [
+        [0,1,0,0],
+        [0,0,0,1],
+        [1,0,1,0],
+        [0,0,0,0]
+    ]
+
+output = generate_successors(initial_state)
+ind = 0 
+for i in output:
+    for j in i:
+        print(j,end="\n")
+    print("----------")
+    ind+=1
+print(ind)
+# test.test_all()
+# print("Test cases passed")
