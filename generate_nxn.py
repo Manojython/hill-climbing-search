70c05b40543d0ee42b5fe84223d1daf23a76cc30
Christian Micklisch <christian.micklisch@successwithsos.com>
2020-03-20 20:48:08 -0400

    Updated the generation file

--- generate_nxn.py
+++ generate_nxn.py
@@ -4,65 +4,58 @@
 # 0 for not queen
 # 1 for queen
 
+import copy
 import random
 import numpy as np
 
 def generate_initial_state(n_queens):
-	board = []
-	for i in range(n_queens):
-		queen_place = random.randint(0,n_queens-1)
-		temp = []
-		counter = 0
-		while counter<n_queens:
-			if(counter==queen_place):
-				temp.append(1)
-			else:
-				temp.append(0)
-			counter+=1
+    board = []
+    for i in range(n_queens):
+        queen_place = random.randint(0,n_queens-1)
+        temp = []
+        counter = 0
+        while counter<n_queens:
+            if(counter==queen_place):
+                temp.append(1)
+            else:
+                temp.append(0)
+            counter+=1
 
-		board.append(temp)
-		return_board = []
-		for i in np.rot90(board):
-			return_board.append(list(i))
+        board.append(temp)
+        return_board = []
+        for i in np.rot90(board):
+            return_board.append(list(i))
 
-	return return_board
+    return return_board
 
 def generate_successors(state):
-	matrix_len = len(state)
-	n_successors = (matrix_len*matrix_len) - matrix_len
-	state = np.rot90(state,axes=(1,0))
-	solution_list = []
-	for i in range(matrix_len):
-		row_name=state[i]
-		counter = 0
-		row_state=[]
-		while counter < len(row_name):
-			if(row_name not in row_state):
-				row_state.append(row_name)
-				counter+=1
-			random.shuffle(row_name)
+    solution_list = []
+    for i in range(len(state)):
+        for j in range(len(state)):
+            local_state = copy.deepcopy(state)
+            for l in range(len(state)):
+                local_state[l][i] = 0
+                
+            local_state[j][i] = 1
+            if (local_state != state):
+                solution_list.append(local_state)
 
-		for j in range(row_state):
-			state[i] = row_state[j]
-			state = np.rot90(state)
-			solution_list.append(state)
-
-	return solution_list
+    return solution_list
 
 
 def generate(n_queens):
-	board= []
-	
-	for i in range(n_queens):
-		temp = []
-		counter = 0
-		while counter<n_queens:
-			temp.append("_")
-			counter+=1
+    board= []
+    
+    for i in range(n_queens):
+        temp = []
+        counter = 0
+        while counter<n_queens:
+            temp.append("_")
+            counter+=1
 
-		board.append(temp)
+        board.append(temp)
 
-	return board
+    return board
 
 
 # print(arr)
--- hill_climbing.py
+++ hill_climbing.py
@@ -1,17 +1,30 @@
 from evaluate import *
+from generate_nxn import *
 
 class Node:
     board = []
-    cost = 10000
+    cost = 9223372036854775807
 
-print(Node())
+def make_node(board):
+    node = Node()
+    node.board = board
+    node.cost = evaluate(board)
+    return node
 
 def find_best_successor(successors):
     best = Node()
     
     for successor in successors:
         if (evaluate(successor) < best.cost):
-            best.board = successor
-            best.cost = evaluate(successor)
+            best = make_node(successor)
 
     return best
+
+def climb(problem):
+    current = make_node(problem)
+    while (1):
+        neighbor = find_best_successor(
+            generate_successors(current.board))
+        if (neighbor.cost >= current.cost):
+            return current.board
+        current = neighbor
